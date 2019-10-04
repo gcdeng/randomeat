f3afb3d54862c014925fb78dc31c8e16f3713754
gcdeng <ericdenggc@gmail.com>
2019-10-04 21:44:44 +0800

    clean up

--- src/App.js
+++ src/App.js
@@ -5,17 +5,17 @@ import getRandomNumber from './util/getRandomNumber';
 import Place from './components/Place/Place';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faDice, faBars} from '@fortawesome/free-solid-svg-icons';
-import { Input, Header, Icon, Image, Menu, Segment, Sidebar, Dropdown, Rating } from 'semantic-ui-react';
+import { Menu, Sidebar, Dropdown, Rating } from 'semantic-ui-react';
 import 'semantic-ui-css/semantic.min.css';
 import './App.css';
 const google = window.google;
 
-const mockLocation = {
-  lat: 25.0823774,
-  lng: 121.57112389999999
-};
+// const mockLocation = {
+//   lat: 25.0823774,
+//   lng: 121.57112389999999
+// };
 
-const mockResult = {"geometry":{"location":{"lat":25.082989,"lng":121.56790490000003},"viewport":{"south":25.08163871970849,"west":121.56660381970846,"north":25.0843366802915,"east":121.56930178029143}},"icon":"https://maps.gstatic.com/mapfiles/place_api/icons/restaurant-71.png","id":"20bf179531a75b13e3267e653f2eb1e4e62e152e","name":"福岡屋拉麵","opening_hours":{"open_now":false},"photos":[{"height":3096,"html_attributions":["<a href=\"https://maps.google.com/maps/contrib/103555148828225958348/photos\">芋台場</a>"],"width":5504}],"place_id":"ChIJgQyg3G6sQjQRiqa5iwMtKLM","plus_code":{"compound_code":"3HM9+55 Taipei, Taiwan","global_code":"7QQ33HM9+55"},"price_level":1,"rating":4,"reference":"ChIJgQyg3G6sQjQRiqa5iwMtKLM","scope":"GOOGLE","types":["restaurant","food","point_of_interest","establishment"],"user_ratings_total":612,"vicinity":"No. 6號, Lane 323, Section 1, Neihu Road, Neihu District","html_attributions":[]};
+// const mockResult = {"geometry":{"location":{"lat":25.082989,"lng":121.56790490000003},"viewport":{"south":25.08163871970849,"west":121.56660381970846,"north":25.0843366802915,"east":121.56930178029143}},"icon":"https://maps.gstatic.com/mapfiles/place_api/icons/restaurant-71.png","id":"20bf179531a75b13e3267e653f2eb1e4e62e152e","name":"福岡屋拉麵","opening_hours":{"open_now":false},"photos":[{"height":3096,"html_attributions":["<a href=\"https://maps.google.com/maps/contrib/103555148828225958348/photos\">芋台場</a>"],"width":5504}],"place_id":"ChIJgQyg3G6sQjQRiqa5iwMtKLM","plus_code":{"compound_code":"3HM9+55 Taipei, Taiwan","global_code":"7QQ33HM9+55"},"price_level":1,"rating":4,"reference":"ChIJgQyg3G6sQjQRiqa5iwMtKLM","scope":"GOOGLE","types":["restaurant","food","point_of_interest","establishment"],"user_ratings_total":612,"vicinity":"No. 6號, Lane 323, Section 1, Neihu Road, Neihu District","html_attributions":[]};
 
 class App extends Component {
   constructor(){
@@ -197,12 +197,12 @@ class App extends Component {
         type: data.value
       });
     }
-    let handleRadiusChange = (e, o) => {
-      console.log(e, o);
-      this.setState({
-        searchRadius: o.value
-      });
-    }
+    // let handleRadiusChange = (e, o) => {
+    //   console.log(e, o);
+    //   this.setState({
+    //     searchRadius: o.value
+    //   });
+    // }
     let handleRate = (e, {rating, maxRating}) => {
       this.setState({
         rating
@@ -261,7 +261,7 @@ class App extends Component {
             <div onClick={()=>this.setSideBarVisible(true)}>
               <FontAwesomeIcon className="icon bars" icon={faBars} />
             </div>
-            <div className="title">randomEat</div>
+            <div className="title">randomeat</div>
             <div onClick={async _ => await this.updateResult()}>
               <FontAwesomeIcon className="icon dice" icon={faDice} />
             </div>
